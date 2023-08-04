5b6bab993e2b7d72347dfc9cd0941a644c3da0ea
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-04 20:19:51 -0300

    Display image.
    Almost there; still glitchy.

--- experiment/src/main.c
+++ experiment/src/main.c
@@ -21,8 +21,8 @@ int main(u16 hard)
     VDP_loadTileSet(logo_aplib.tileset, ind, DMA);
     ind += logo_aplib.tileset->numTile;
     int idx2 = ind;
-    VDP_loadTileSet(logo_ucomp.tileset, ind, DMA);
-    ind += logo_ucomp.tileset->numTile;
+    VDP_loadTileSet(frame_94.tileset, ind, DMA);
+    ind += frame_94.tileset->numTile;
 
     // This one is not packed
     TileMap *utmap = logo_ucomp.tilemap;
@@ -31,10 +31,10 @@ int main(u16 hard)
 
     // draw backgrounds
     VDP_setTileMapEx(BG_A, ctmap, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx1), 0, 0,  0, 0, 30, 12, CPU);
-    VDP_setTileMapEx(BG_A, utmap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, idx2), 0, 12, 0, 0, 30, 12, CPU);
+    VDP_setTileMapEx(BG_A, utmap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, idx2), 0, 12, 0, 0, frame_94.tilemap->w, frame_94.tilemap->h, CPU);
 
     VDP_setPaletteColors(0,  (u16*)logo_aplib.palette->data, 16);
-	VDP_setPaletteColors(16, (u16*)logo_ucomp.palette->data, 32);
+	VDP_setPaletteColors(16, (u16*)frame_94.palette->data, 32);
 
     // VDP process done, we can re enable interrupts
     SYS_enableInts();
