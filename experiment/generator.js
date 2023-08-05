202bdc3b473c26202278f2bcd703cb9602f0122f
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-05 07:35:45 -0300

    Generate `.h` file.

---
+++ experiment/generator.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+//fs.readdirSync('tmpmv_test');
+//parseInt(/^\w+_(\d+)\.png$/.exec('frame_142.png')[1])
+const MOVIE_DIR = 'tmpmv_test/';
+const GENSRC_DIR = 'src/generated/';
+
+const FILE_REGEX = /^\w+_(\d+)\.png$/;
+const fileNames = fs.readdirSync(MOVIE_DIR).filter(s => FILE_REGEX.test(s));
+const sortedFileNames = fileNames
+	.map(name => ({ idx: parseInt(FILE_REGEX.exec(name)[1]), name }))
+	.sort((a, b) => a.idx - b.idx)
+	.map(o => o.name);
+	
+if (!fs.existsSync(GENSRC_DIR)) {
+	fs.mkdirSync(GENSRC_DIR, { recursive: true });
+}
+
+fs.writeFileSync(`${GENSRC_DIR}/movie_res.h`, `
+#ifndef _MOVIE_RES_H
+#define _MOVIE_RES_H
+
+#include <genesis.h>
+
+typedef struct MovieData {
+	u16 frameCount,
+	Image *frames[];
+} MovieData;
+
+extern const MovieData movie_test;
+
+#endif _MOVIE_RES_H
+`);
