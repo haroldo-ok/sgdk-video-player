b2f67e1a547825f5a6e1649aed15184d0c7f62bd
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-05 08:47:12 -0300

    Display the frames, roughly.

--- experiment/src/main.c
+++ experiment/src/main.c
@@ -3,6 +3,8 @@
 #include "gfx.h"
 #include "dma.h"
 
+#include "generated/movie_res.h"
+
 int main(u16 hard)
 {
     u16 ind;
@@ -21,7 +23,7 @@ int main(u16 hard)
     VDP_loadTileSet(logo_aplib.tileset, ind, DMA);
     ind += logo_aplib.tileset->numTile;
     int idx2 = ind;
-    VDP_loadTileSet(frame_94.tileset, ind, DMA);
+//    VDP_loadTileSet(frame_94.tileset, ind, DMA);
     ind += frame_94.tileset->numTile;
 
     // This one is not packed
@@ -31,16 +33,25 @@ int main(u16 hard)
 
     // draw backgrounds
     VDP_setTileMapEx(BG_A, ctmap, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx1), 0, 0,  0, 0, 30, 12, DMA);
-    VDP_setTileMapEx(BG_A, frame_94.tilemap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, idx2), 0, 12, 0, 0, frame_94.tilemap->w, frame_94.tilemap->h, DMA);
+//    VDP_setTileMapEx(BG_A, frame_94.tilemap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, idx2), 0, 12, 0, 0, frame_94.tilemap->w, frame_94.tilemap->h, DMA);
 
     VDP_setPaletteColors(0,  (u16*)logo_aplib.palette->data, 16);
-	VDP_setPaletteColors(16, (u16*)frame_94.palette->data, 32);
+//	VDP_setPaletteColors(16, (u16*)frame_94.palette->data, 32);
 
     // VDP process done, we can re enable interrupts
     SYS_enableInts();
 
     while(TRUE)
     {
+		for (u16 frameNumber = 0; frameNumber != movie_test.frameCount; frameNumber++) {
+			const Image *frame = movie_test.frames[frameNumber];
+
+			VDP_waitVInt();
+			VDP_loadTileSet(frame->tileset, idx2, DMA);
+			VDP_setTileMapEx(BG_A, frame->tilemap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, idx2), 0, 12, 0, 0, frame->tilemap->w, frame->tilemap->h, DMA);
+			VDP_setPaletteColors(16, (u16*)frame->palette->data, 32);
+		}
+		
         VDP_waitVInt();
     }
 
