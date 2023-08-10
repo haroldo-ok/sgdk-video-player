96d5df23d03bca7c3d9af65f64c121704c58f3b6
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-10 19:27:24 -0300

    Implement call for the tile reduction script

--- experiment/convert.bat
+++ experiment/convert.bat
@@ -1,4 +1,7 @@
 rmdir /s /q tmpmv_test
 md tmpmv_test
-ffmpeg -i "Genesis does what Nintendon't - v2 HD by RVGM.mp4" -r 12 -s 256x112 tmpmv_test/frame_%%d.jpg
-mogrify -path tmpmv_test -colors 16 -format png tmpmv_test\*.jpg
+ffmpeg -i "Genesis does what Nintendon't - v2 HD by RVGM.mp4" -r 12 -s 256x224 tmpmv_test/frame_%%d.jpg
+node --max-old-space-size=4096 reduce.js
+mogrify -path tmpmv_test -colors 16 -format png tmpmv_test\*.png
+node generator.js
+
