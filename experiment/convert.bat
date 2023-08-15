8fd3cf47cd1830c74423a39d071e919bd0fa5b7e
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-14 21:25:47 -0300

    Extract sound.

--- experiment/convert.bat
+++ experiment/convert.bat
@@ -1,6 +1,6 @@
 rmdir /s /q tmpmv_test
 md tmpmv_test
-ffmpeg -i "Genesis does what Nintendon't - v2 HD by RVGM.mp4" -r 12 -s 320x224 tmpmv_test/frame_%%d.jpg
+ffmpeg -i "Genesis does what Nintendon't - v2 HD by RVGM.mp4" -r 12 -s 320x224 tmpmv_test/frame_%%d.jpg  -ar 22050 -ac 1 -acodec pcm_u8 tmpmv_test/sound.wav
 node --max-old-space-size=4096 reduce.js
 mogrify -path tmpmv_test -colors 16 -format png tmpmv_test\*.png
 node generator.js
