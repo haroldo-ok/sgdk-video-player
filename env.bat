13fbc8a2faf0bc4cdc5e29ba07b594ef5f926e62
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-17 19:30:27 -0300

    Update environment vars.

--- env.bat
+++ env.bat
@@ -1,1 +1,2 @@
-path %PATH%;D:\Pessoal\Projetos\ImageMagick-7.0.10-53-portable-Q16-x86;D:\Pessoal\Projetos\sgdk170\bin
+set IMAGEMAGICK=D:\Pessoal\Projetos\ImageMagick-7.0.10-53-portable-Q16-x86
+path %PATH%;%IMAGEMAGICK%;D:\Pessoal\Projetos\sgdk170\bin
