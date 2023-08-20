873aff5135920f5e5b53d4cdb597dc0d8e67acb3
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-20 16:54:44 -0300

    Clear directory before conversion.

--- js/convert.js
+++ js/convert.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 const { extractVideoFrames } = require('./execute');
 
 const checkFileExists = async fileName => fs.promises.access(fileName, fs.constants.F_OK)
    .then(() => true)
    .catch(() => false);
+   
+const clearDir = async dir => {
+	for (const fileName of await fs.promises.readdir(dir)) {
+		await fs.promises.unlink(path.join(dir, fileName));
+	}
+};
 
 const convertVideo = async (srcVideo, destDir, { imagemagickDir }) => {
 	console.log('convertVideo', { srcVideo, destDir, imagemagickDir });
@@ -19,6 +26,8 @@ const convertVideo = async (srcVideo, destDir, { imagemagickDir }) => {
 		await fs.promises.mkdir(destDir, { recursive: true });
 	}
 	
+	await clearDir(destDir);
+		
 	await extractVideoFrames(srcVideo, destDir, { imagemagickDir });
 }
 
