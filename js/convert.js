845341243cadeac4629e59e9ac1f708b709f320a
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-20 15:20:53 -0300

    Create target dir, if missing.

--- js/convert.js
+++ js/convert.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const fs = require('fs');
+
+const checkFileExists = async fileName => fs.promises.access(fileName, fs.constants.F_OK)
+   .then(() => true)
+   .catch(() => false);
+
+
 const convertVideo = async (srcVideo, destDir, options) => {
 	console.log('convertVideo', { srcVideo, destDir, options });
+	
+	if (!await checkFileExists(srcVideo)) {
+		throw new Error(`Input video not found: ${srcVideo}`);
+	}
+
+	if (!await checkFileExists(destDir)) {
+		await fs.promises.mkdir(destDir, { recursive: true });
+	}
 }
 
 module.exports = { convertVideo };
