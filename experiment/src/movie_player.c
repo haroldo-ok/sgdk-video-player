764af96515c78a12185eed697bf34c522b5bee53
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-27 20:13:32 -0300

    Refactor movie playing routine into a function.

--- experiment/src/movie_player.c
+++ experiment/src/movie_player.c
@@ -1,24 +1,15 @@
 #include <genesis.h>
 
-#include "gfx.h"
-#include "sound.h"
-
-#include "dma.h"
-
 #include "movie_player.h"
-#include "movie_HD_SEGA_logo_4x3_mp4.h"
 
 /// Counter to be incremented by background task
-static volatile uint32_t hwFrameCount = 0;
+static volatile uint32_t movieFrameCount = 0;
 
-extern const MovieData movie_HD_SEGA_logo_4x3_mp4;
-
-void VIntHandler() {
-	hwFrameCount++;
+void MVP_VIntHandler() {
+	movieFrameCount++;
 }
 
-int main(u16 hard)
-{
+void MVP_playMovie(const MovieData *movie) {
     // disable interrupt when accessing VDP
     SYS_disableInts();
 	
@@ -29,64 +20,64 @@ int main(u16 hard)
     VDP_setPaletteColors(0, (u16*) palette_black, 64);
 
 	// Set up frame counter
-	SYS_setVIntCallback(VIntHandler);
+	SYS_setVIntCallback(MVP_VIntHandler);
 
     // VDP process done, we can re enable interrupts
     SYS_enableInts();
 	
 	bool activeBuffer = FALSE;
 	u16 idx1 = TILE_USERINDEX;
-	u16 idx2 = idx1 + movie_HD_SEGA_logo_4x3_mp4.w * movie_HD_SEGA_logo_4x3_mp4.h;
+	u16 idx2 = idx1 + movie->w * movie->h;
 	
 	u16 videoFrameRate = 12;
 	u16 systemFrameRate = IS_PALSYSTEM ? 50 : 60;
 
-    while(TRUE)
-    {
-		u16 videoFrame = 0;
+	u16 videoFrame = 0;
 
-		SYS_disableInts();
-		hwFrameCount = 0;
-		SYS_enableInts();
+	SYS_disableInts();
+	movieFrameCount = 0;
+	SYS_enableInts();
 
-		SND_startPlay_2ADPCM(movie_HD_SEGA_logo_4x3_mp4.sound, movie_HD_SEGA_logo_4x3_mp4.soundLen, SOUND_PCM_CH1, FALSE);
+	SND_startPlay_2ADPCM(movie->sound, movie->soundLen, SOUND_PCM_CH1, FALSE);
+	
+	while (videoFrame < movie->frameCount) {
+		const Image *frame = movie->frames[videoFrame];
+		u16 idx = activeBuffer ? idx1 : idx2;
+		u16 palNum = activeBuffer ? PAL0 : PAL1;
+		u16 palIdx = activeBuffer ? 0 : 16;
+
+		VDP_loadTileSet(frame->tileset, idx, DMA_QUEUE);
 		
-		while (videoFrame < movie_HD_SEGA_logo_4x3_mp4.frameCount) {
-			const Image *frame = movie_HD_SEGA_logo_4x3_mp4.frames[videoFrame];
-			u16 idx = activeBuffer ? idx1 : idx2;
-			u16 palNum = activeBuffer ? PAL0 : PAL1;
-			u16 palIdx = activeBuffer ? 0 : 16;
-
-			VDP_loadTileSet(frame->tileset, idx, DMA_QUEUE);
+		TileMap *ctmap = unpackTileMap(frame->tilemap, NULL);
+		VDP_setTileMapEx(BG_A, ctmap, TILE_ATTR_FULL(palNum, FALSE, FALSE, FALSE, idx), 0, 0, 0, 0, frame->tilemap->w, frame->tilemap->h, DMA_QUEUE);
+		
+		VDP_waitVInt();
+		
+		DMA_flushQueue();
+		VDP_setPaletteColors(palIdx, (u16*)frame->palette->data, palIdx + 16);
+		MEM_free(ctmap);
+		
+		activeBuffer = !activeBuffer;
+		
+		// Waits for next video frame
+		
+		u16 previousVideoFrame = videoFrame;
+		while (videoFrame == previousVideoFrame) {
+			SYS_disableInts();
+			uint32_t frameCount = movieFrameCount;
+			SYS_enableInts();
 			
-			TileMap *ctmap = unpackTileMap(frame->tilemap, NULL);
-			VDP_setTileMapEx(BG_A, ctmap, TILE_ATTR_FULL(palNum, FALSE, FALSE, FALSE, idx), 0, 0, 0, 0, frame->tilemap->w, frame->tilemap->h, DMA_QUEUE);
-			
-			VDP_waitVInt();
-			
-			DMA_flushQueue();
-			VDP_setPaletteColors(palIdx, (u16*)frame->palette->data, palIdx + 16);
-			MEM_free(ctmap);
-			
-			activeBuffer = !activeBuffer;
-			
-			// Waits for next video frame
-			
-			u16 previousVideoFrame = videoFrame;
-			while (videoFrame == previousVideoFrame) {
-				SYS_disableInts();
-				uint32_t frameCount = hwFrameCount;
-				SYS_enableInts();
-				
-				videoFrame = frameCount * videoFrameRate / systemFrameRate;
-				if (videoFrame == previousVideoFrame) {
-					VDP_waitVInt();
-				}
+			videoFrame = frameCount * videoFrameRate / systemFrameRate;
+			if (videoFrame == previousVideoFrame) {
+				VDP_waitVInt();
 			}
 		}
-		
-        VDP_waitVInt();
-    }
+	}
+	
+	VDP_waitVInt();
 
-    return 0;
+	// Remove frame counter
+    SYS_disableInts();
+	SYS_setVIntCallback(NULL);
+	SYS_enableInts();
 }
